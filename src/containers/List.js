821fc428aded273fa4e6a0aca8589b87b81f3388
Francisco <franciscomanuel.gonzalez3@gmail.com>
2021-04-12 22:23:14 -0300

    project structure

---
+++ src/containers/List.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Card from '../components/Card/Card';
+
+const List = () => {
+    return (
+        <Card />
+    )
+}
+
+export default List
+
--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log('hello world')
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import List from './containers/List';
+
+ReactDOM.render(<List />, document.getElementById('root'))
