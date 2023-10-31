a5650a447a92a16f87e63897173dbdb48b5d5479
franciscog98 <franciscomanuel.gonzalez3@gmail.com>
2023-10-31 20:58:51 -0300

    feat: add form component to search movies

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,29 +2,37 @@ import { useState } from "react";
 import "./formStyles.scss";
 
 const Form = () => {
-
   const [inputSearch, setInputSearch] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(inputSearch);
-  }
+  };
 
   return (
-    <form className="form_container">
-      <fieldset className="form_fieldset">
-        <label htmlFor="serach">Search your favorite series or movie</label>
+    <form className="form_container" onSubmit={(e) => handleSubmit(e)}>
+
+      <label htmlFor="serach">Search your favorite series or movie:</label>
+
+      <br />
+
+      <fieldset>
         <input
           id="serach"
           type="text"
+          name="serach"
           placeholder="Search your favorite series or movie"
           value={inputSearch}
           onChange={(e) => setInputSearch(e.target.value)}
+          autoFocus='1'
+          required
+          aria-label="Search your favorite series or movie"
         />
-        <input type="button" value={"Search"} onClick={(e) => handleSubmit(e)} />
+
+        <button type="submit" aria-labelledby="Search movie or serie" role="button">Search</button>
       </fieldset>
     </form>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
