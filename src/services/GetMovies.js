74be1c1cab1e0c8ad783cae607ac7b7a120efe72
franciscog98 <franciscomanuel.gonzalez3@gmail.com>
2023-11-10 19:28:33 -0300

    fix: add /dist folder and edit gitignore

--- src/services/GetMovies.js
+++ src/services/GetMovies.js
@@ -1,8 +1,8 @@
-const OMDB_API = `http://www.omdbapi.com/?apikey=${import.meta.env.VITE_API_KEY}&s=`;
+const OMDB_API_URL = `http://www.omdbapi.com/?apikey=${import.meta.env.VITE_API_KEY}&s=`;
 
 const GetMovies = (inputSearch) => {
 
-  const res = fetch(OMDB_API + inputSearch)
+  const res = fetch(OMDB_API_URL + inputSearch)
     .then(res => res.json())
     .then(data => data.Search)
     .catch(err => console.log('Error: ', err))
