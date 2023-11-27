5c0c00ae44b9529669cf4031eacfccd6ba600ad0
franciscog98 <franciscomanuel.gonzalez3@gmail.com>
2023-11-26 22:54:36 -0300

    fix: hope this work

--- src/services/GetMovies.js
+++ src/services/GetMovies.js
@@ -1,9 +1,8 @@
-// const OMDB_API_URL = `https://www.omdbapi.com/?apikey=${import.meta.env.VITE_API_KEY}&s=`;
+const OMDB_API_URL = `https://www.omdbapi.com/?apikey=${import.meta.env.VITE_API_KEY}&s=`;
 
 const GetMovies = (inputSearch) => {
 
-  // const res = fetch(OMDB_API_URL + inputSearch)
-  const res = fetch(`https://www.omdbapi.com/?apikey=${import.meta.env.VITE_API_KEY}&s=${inputSearch}`)
+  const res = fetch(`${OMDB_API_URL + inputSearch}`)
     .then(res => res.json())
     .then(data => data.Search)
     .catch(err => console.log('Error: ', err))
